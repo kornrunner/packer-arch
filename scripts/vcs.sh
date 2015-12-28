49f245b90c6be88a9b0263b1014e0350d8494cee
Boris Momčilović <boris@firstbeatmedia.com>
2015-12-28 03:14:23 +0100

    vcs: gitflow-avh, autocompletion & git up

--- scripts/vcs.sh
+++ scripts/vcs.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-/usr/bin/runuser -l vagrant -c '/usr/bin/yaourt -S --noconfirm git gitflow-git subversion'
+/usr/bin/runuser -l vagrant -c '/usr/bin/yaourt -S --noconfirm git gitflow-avh gitflow-zshcompletion-avh subversion python-pip'
+/usr/bin/pip install git-up
