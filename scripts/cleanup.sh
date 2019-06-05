006e8d887019ce652bfe97134c275bbe52010453
Boris Momčilović <boris@firstbeatmedia.com>
2019-06-05 15:22:26 +0200

    scripts: cleanup

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -4,6 +4,6 @@
 /usr/bin/paccache -rk0
 /usr/bin/pacman -Rcns --noconfirm gptfdisk
 /usr/bin/pacman -Scc --noconfirm
-/usr/bin/yay -Sc --noconfirm
-/usr/bin/yay -Scc --noconfirm
+/usr/bin/runuser -l vagrant -c '/usr/bin/yay -Sc --noconfirm'
+/usr/bin/runuser -l vagrant -c '/usr/bin/yay -Scc --noconfirm'
 rm /tmp/yay-* -Rf
